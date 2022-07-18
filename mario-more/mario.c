77493d9ab1aeff3ac50f20d15576875bd87f4874
heddxh <63106571+heddxh@users.noreply.github.com>
2022-07-18 14:10:37 +0000

    Mon, Jul 18, 2022, 2:10 PM +00:00

--- mario-more/mario.c
+++ mario-more/mario.c
@@ -8,18 +8,25 @@ int main(void)
     {
         h = get_int("Height: ");
     }
-    while (h > 1); //ensure a valid input
+    while (h < 1); //ensure a valid input
 
-    for (int i = 1, i < h + 1, i++)
+    for (int i = 1; i < h + 1; i++)
     {
-        for (int j = 1, j < h - i + 1, j++)
+        for (int j = 1; j < h - i + 1; j++)
         {
             printf(" ");
         }
 
-        for (int k = 1, k < i + 1, k++)
+        for (int k = 1; k < i + 1; k++)
         {
             printf("#");
         }
+
+        for (int k = 1; k < i + 1; k++)
+        {
+            printf("#");
+        }
+        printf("\n");
     }
+
 }
