1f4590d25cfa5a1aad3f709e1fe59fbeca2e7de0
heddxh <63106571+heddxh@users.noreply.github.com>
2022-07-21 08:20:21 +0000

    Thu, Jul 21, 2022, 8:20 AM +00:00

--- credit/credit.c
+++ credit/credit.c
@@ -1,22 +1,30 @@
 #include <cs50.h>
 #include <stdio.h>
 
-long get_numbers(void);
-int multiply_sum(long numbers);
-int remain_sum(long numbers);
+long get_number(void);
+int multiply_sum(long number);
+int remain_sum(long number);
 
 
 int main(void)
 {
     // user input
-    long numbers = get_numbers();
+    long number = get_number();
 
     // calculate
-    int sum1 = multiply_sum(numbers);
-    int sum2 = remain_sum(numbers);
+    int sum1 = multiply_sum(number);
+    int sum2 = remain_sum(number);
 
     // plus
     int result = sum1 + sum2
+
+    // mount length
+    int length = 0;
+    while(number > 0)
+    {
+        length++;
+        number = number / 10
+    }
 
     // judge
     if(result % 10 = 0)
@@ -26,26 +34,26 @@ int main(void)
 
 }
 
-long get_numbers(void)
+long get_number(void)
 {
-    long numbers;
+    long number;
     do
     {
-        numbers = get_long("Number: ");
+        number = get_long("Number: ");
     }
-    while(numbers < 0);
-    return numbers;
+    while(number < 0);
+    return number;
 }
 
-int multiply_sum(long numbers)
+int multiply_sum(long number)
 {
     int sum;
     int digit;
-    while(numbers >= 10)
+    while(number >= 10)
     {
-        numbers = numbers / 10;
-        digit = numbers % 10;
-        numbers = numbers / 10;
+        number = number / 10;
+        digit = number % 10;
+        number = number / 10;
         if (digit * 2 > 9)
         {
             sum = sum + digit * 2 / 10 + digit * 2 % 10;
@@ -58,14 +66,14 @@ int multiply_sum(long numbers)
     }
 }
 
-int remain_sum(long numbers)
+int remain_sum(long number)
 {
     int digit;
     int sum;
-    while(numbers >= 0)
+    while(number >= 0)
     {
-        digit = numbers % 10;
-        numbers = numbers / 100;
+        digit = number % 10;
+        number = number / 100;
         sum = sum + digit
     }
     return sum;
