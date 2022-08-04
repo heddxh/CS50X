0cf11b7a2f8eecff4216160102daae71c9710273
heddxh <63106571+heddxh@users.noreply.github.com>
2022-08-04 15:32:07 +0000

    Thu, Aug 4, 2022, 3:32 PM +00:00

--- week2/substitution/substitution.c
+++ week2/substitution/substitution.c
@@ -4,8 +4,17 @@
 
 int main(int argc, string argv[])
 {
-    if (argv[1] == NULL)
+    if (argv[1] == NULL || argv[2] != NULL)
     {
-        printf("");
+        printf("Usage: ./substitution key\n");
+        return 1;
     }
+    else if (argc != 26)
+    {
+        printf("Key must contain 26 characters.\n");
+        return 1;
+    }
+
+    
+
 }
