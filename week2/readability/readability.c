a30eea0b2a990d5177013f04c3d8d7a6c745a703
heddxh <63106571+heddxh@users.noreply.github.com>
2022-08-04 09:25:10 +0000

    Thu, Aug 4, 2022, 9:25 AM +00:00

--- week2/readability/readability.c
+++ week2/readability/readability.c
@@ -2,14 +2,34 @@
 #include <stdio.h>
 #include <ctype.h>
 #include <string.h>
+#include <math.h>
 
+int count_letters(string text);
 int count_words(string text);
+int count_sentences(string text);
 
 int main(void)
 {
     string text = get_string("Text: ");
+    int l = get_letters(text);
+    int w = get_words(text);
+    int s = get_sentences(text);
 
+    float L = l / (w / 100.0);
+    
+}
 
+int count_letters(string text)
+{
+    int count = 0;
+    for (int i = 0, n = strlen(text); i < n; i++)
+    {
+        if (isalpha(text[i]))
+        {
+            count++;
+        }
+    }
+    return count;
 }
 
 int count_words(string text)
@@ -23,4 +43,17 @@ int count_words(string text)
         }
     }
     return count + 1;
+}
+
+int count_sentences(string text)
+{
+    int count = 0;
+    for (int i = 0, n = strlen(text); i < n; i++)
+    {
+        if (text[i] == '?' || text[i] == '.' || text[i] == '!')
+        {
+            count++;
+        }
+    }
+    return count;
 }
