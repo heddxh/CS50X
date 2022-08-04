767ca833c9a7ac69fb8d59284793d5737d64aa87
heddxh <63106571+heddxh@users.noreply.github.com>
2022-08-04 05:06:01 +0000

    Thu, Aug 4, 2022, 5:06 AM +00:00

--- week2/readability/readability.c
+++ week2/readability/readability.c
@@ -3,16 +3,16 @@
 #include <ctype.h>
 #include <string.h>
 
-int count_letters(string text);
+int count_words(string text);
 
 int main(void)
 {
     string text = get_string("Text: ");
-    printf("count letters: %c\n", count_letters(text));
+    printf("count words: %c\n", count_words(text));
 
 }
 
-int count_letters(string text)
+int count_words(string text)
 {
     int count = 0;
     for (int i = 0, n = strlen(text); i < n; i++)
