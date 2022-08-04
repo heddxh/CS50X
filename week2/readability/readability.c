eb48344669e819e1b3513860f824492902249020
heddxh <63106571+heddxh@users.noreply.github.com>
2022-08-04 05:00:21 +0000

    Thu, Aug 4, 2022, 5:00 AM +00:00

--- week2/readability/readability.c
+++ week2/readability/readability.c
@@ -1,12 +1,14 @@
 #include <cs50.h>
 #include <stdio.h>
 #include <ctype.h>
+#include <string.h>
 
 int count_letters(string text);
 
 int main(void)
 {
     string text = get_string("Text: ");
+    printf("count letters: %c\n", count_letters(text));
 
 }
 
@@ -15,6 +17,10 @@ int count_letters(string text)
     int count = 0;
     for (int i = 0, n = strlen(text); i < n; i++)
     {
-        if text[i] == 
+        if (isspace(text[i]) != 0)
+        {
+            count++;
+        }
     }
+    return count;
 }
