de98ea02742f493d778d4cf5bb5b1be1bb0b62b6
heddxh <63106571+heddxh@users.noreply.github.com>
2022-08-04 16:06:58 +0000

    Thu, Aug 4, 2022, 4:06 PM +00:00

--- week2/substitution/substitution.c
+++ week2/substitution/substitution.c
@@ -18,16 +18,27 @@ int main(int argc, string argv[])
 
     string raw = get_string("plaintext: ");
     string key = argv[1];
-    string cip;
+    string cip = NULL;
     for (int i = 0, n = strlen(raw); i < n; i++)
     {
         if (isalpha(raw[i]))
         {
             if(islower(raw[i]))
             {
-                cip[i] = raw[i]
+                int index = raw[i] - 97;
+                cip[i] = tolower(key[index]);
             }
+            else
+            {
+                int index = raw[i] - 65;
+                cip[i] = toupper(key[index]);
+            }
+        }
+        else
+        {
+            cip[i] = raw[i];
         }
     }
 
+    printf("ciphertext: %s\n", cip);
 }
