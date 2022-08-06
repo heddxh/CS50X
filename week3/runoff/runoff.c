2149240e9f8e91266f10d23932b3dd6548d45198
heddxh <63106571+heddxh@users.noreply.github.com>
2022-08-06 17:12:53 +0000

    Sat, Aug 6, 2022, 5:12 PM +00:00

--- week3/runoff/runoff.c
+++ week3/runoff/runoff.c
@@ -143,7 +143,14 @@ void tabulate(void)
 {
     for (int i = 0; i < voter_count; i++)
     {
-        if (preferences)
+        for (int j = 0; j < candidate_count; j++)
+        {
+            if (!candidates[preferences[i][j]].eliminated)
+            {
+                candidates[preferences[i][j]].votes++;
+                break;
+            }
+        }
     }
     return;
 }
