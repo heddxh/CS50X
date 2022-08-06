4881365b50b3deea827bbabf8c883d610dc5bde8
heddxh <63106571+heddxh@users.noreply.github.com>
2022-08-06 16:32:26 +0000

    Sat, Aug 6, 2022, 4:32 PM +00:00

--- week3/plurality/plurality.c
+++ week3/plurality/plurality.c
@@ -93,7 +93,7 @@ void print_winner(void)
     {
         if (candidates[i].votes == max)
         {
-            printf("candidates[i].name\n");
+            printf("%s\n", candidates[i].name);
         }
     }
 
