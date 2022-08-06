1b9d562cd6781a5ab3bc4c66fc1fe844bc89a323
heddxh <63106571+heddxh@users.noreply.github.com>
2022-08-06 17:05:54 +0000

    Sat, Aug 6, 2022, 5:05 PM +00:00

--- week3/runoff/runoff.c
+++ week3/runoff/runoff.c
@@ -131,7 +131,7 @@ bool vote(int voter, int rank, string name)
     {
         if (!strcmp(name, candidates[voter].name))
         {
-            
+            preferences[voter][rank] = i;
             return true;
         }
     }
