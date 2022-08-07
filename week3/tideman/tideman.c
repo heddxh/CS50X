6502b4c2ecd37cc36e80d28bf2db0b14886bd382
heddxh <63106571+heddxh@users.noreply.github.com>
2022-08-07 15:12:58 +0000

    Sun, Aug 7, 2022, 3:12 PM +00:00

--- week3/tideman/tideman.c
+++ week3/tideman/tideman.c
@@ -116,8 +116,10 @@ void record_preferences(int ranks[])
 {
     for (int i = 0; i < candidate_count; i++)
     {
-        for (int)
-
+        for (int j = 0; j < candidate_count; j++)
+        {
+            
+        }
     }
     return;
 }
