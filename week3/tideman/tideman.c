fec62c215ed4e8187e481b44584a2ac5315ae17d
heddxh <63106571+heddxh@users.noreply.github.com>
2022-08-08 03:49:51 +0000

    Mon, Aug 8, 2022, 3:49 AM +00:00

--- week3/tideman/tideman.c
+++ week3/tideman/tideman.c
@@ -152,6 +152,7 @@ void sort_pairs(void)
     for (int i = 0; i < pair_count; i++)
     {
         int min = preferences[pairs[i].winner][pairs[i].loser];
+        k = i;
         // Find smallest
         for (int j = i + 1; j < pair_count; j++)
         {
