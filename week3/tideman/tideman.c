6589e65b8ffeea45759512258cb2dcf858621a59
heddxh <63106571+heddxh@users.noreply.github.com>
2022-08-08 04:21:41 +0000

    Mon, Aug 8, 2022, 4:21 AM +00:00

--- week3/tideman/tideman.c
+++ week3/tideman/tideman.c
@@ -182,7 +182,7 @@ bool will_cycle(int winner, int loser)
     {
         if (locked[loser][i])
         {
-            if (will_cycle([winner][i])) // Recursion is difficult...
+            if (will_cycle(winner, i)) // Recursion is difficult...
             {
                 return true;
             }
