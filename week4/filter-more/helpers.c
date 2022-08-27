0eae4b1cb41448b98a77d0cd98997b73459bffcb
heddxh <63106571+heddxh@users.noreply.github.com>
2022-08-27 09:32:28 +0000

    Sat, Aug 27, 2022, 9:32 AM +00:00

--- week4/filter-more/helpers.c
+++ week4/filter-more/helpers.c
@@ -3,6 +3,17 @@
 // Convert image to grayscale
 void grayscale(int height, int width, RGBTRIPLE image[height][width])
 {
+    for (int h = 0; h < height; h++)
+    {
+        for (int w = 0; w < 0; w++)
+        {
+            BYTE B = image[h][w].rgbtBlue;
+            BYTE G = image[h][w].rgbtGreen;
+            BYTE R = image[h][w].rgbtRed;
+
+            
+        }
+    }
     return;
 }
 
