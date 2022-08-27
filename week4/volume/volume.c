2f2aef51019dab642be3209cd69cc184ad0adb96
heddxh <63106571+heddxh@users.noreply.github.com>
2022-08-27 03:05:47 +0000

    Sat, Aug 27, 2022, 3:05 AM +00:00

--- week4/volume/volume.c
+++ week4/volume/volume.c
@@ -33,7 +33,8 @@ int main(int argc, char *argv[])
 
     float factor = atof(argv[3]);
 
-    // TODO: Copy header from input file to output file
+    // Copy header from input file to output file
+    output
 
     // TODO: Read samples from input file and write updated data to output file
 
