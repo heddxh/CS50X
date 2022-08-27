9ecbacabca711fd68e3dbb304679d2eaeb2e75fc
heddxh <63106571+heddxh@users.noreply.github.com>
2022-08-27 04:59:31 +0000

    Sat, Aug 27, 2022, 4:59 AM +00:00

--- week4/volume/volume.c
+++ week4/volume/volume.c
@@ -35,8 +35,7 @@ int main(int argc, char *argv[])
 
     // Copy header from input file to output file
     uint8_t header[HEADER_SIZE];
-
-    int cnt = fread(header, sizeof(uint8_t), 1, input);
+    fread(header, sizeof(uint8_t), 1, input);
     fwrite(header, sizeof(uint8_t), 1, output);
 
     // Read samples from input file and write updated data to output file
