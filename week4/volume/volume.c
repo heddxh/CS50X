53c2db4dd48a28473eed86b525d963c4d02527b3
heddxh <63106571+heddxh@users.noreply.github.com>
2022-08-27 03:14:49 +0000

    Sat, Aug 27, 2022, 3:14 AM +00:00

--- week4/volume/volume.c
+++ week4/volume/volume.c
@@ -34,7 +34,12 @@ int main(int argc, char *argv[])
     float factor = atof(argv[3]);
 
     // Copy header from input file to output file
-    uint8_t header[n];
+    uint8_t header[HEADER_SIZE];
+    malloc
+    for (int i = 0; i < HEADER_SIZE; i++)
+    {
+        header[i] = fread(input, sizeof(uint8_t), 1, input)
+    }
     output
 
     // TODO: Read samples from input file and write updated data to output file
