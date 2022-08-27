5ab37e7d37b7607f93289c19d0d6aa31a8032bb8
heddxh <63106571+heddxh@users.noreply.github.com>
2022-08-27 03:17:52 +0000

    Sat, Aug 27, 2022, 3:17 AM +00:00

--- week4/volume/volume.c
+++ week4/volume/volume.c
@@ -35,12 +35,12 @@ int main(int argc, char *argv[])
 
     // Copy header from input file to output file
     uint8_t header[HEADER_SIZE];
-    uint8_t *output_pointer = malloc()
+    uint8_t *output_pointer = malloc(HEADER_SIZE);
     for (int i = 0; i < HEADER_SIZE; i++)
     {
-        header[i] = fread(input, sizeof(uint8_t), 1, input)
+        header[i] = fread(out_pointer, sizeof(uint8_t), 1, input);
     }
-    output
+    
 
     // TODO: Read samples from input file and write updated data to output file
 
