6dca677c2842de68d951a7ad75ef2977bcd4c2f7
heddxh <63106571+heddxh@users.noreply.github.com>
2022-10-06 14:33:25 +0000

    Thu, Oct 6, 2022, 2:33 PM +00:00

--- week4/recover/recover.c
+++ week4/recover/recover.c
@@ -27,7 +27,7 @@ int main(int argc, char *argv[])
     {
         if (buffer[0] == 0xff && buffer[1] == 0xd8 && buffer[2] == 0xff)
         {
-            if (buffer[3] >= 0xe0 && buffer[3] <= 0xef)
+            if (buffer[3] & 0x0f == 0xef)
             {
                 sprint(filename, )
             }
