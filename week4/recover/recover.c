896a08e512cb2a22c95822545c883d2a425819b5
heddxh <63106571+heddxh@users.noreply.github.com>
2022-10-06 13:17:35 +0000

    Thu, Oct 6, 2022, 1:17 PM +00:00

--- week4/recover/recover.c
+++ week4/recover/recover.c
@@ -3,16 +3,22 @@
 
 int main(int argc, char *argv[])
 {
+    const int BLOCK_SIZE = 512;
+
     if (argc != 2)
     {
         printf("Usage: ./recover IMAGE");
         return 1;
     }
 
-    const int BLOCK_SIZE = 512;
-
     FILE *raw_file = fopen(argv[1], "r");
-    int *buffer = 
+    if (raw_file == NULL)
+    {
+        printf("Could not open file.\n");
+        return 1;
+    }
+
+    int *buffer =
     while (fread(buffer, 1, BLOCK_SIZE, raw_file) == BLOCK_SIZE)
     {
 
