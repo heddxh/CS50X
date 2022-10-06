9e565b7d5816e94ebea1e4f950a13296332858f0
heddxh <63106571+heddxh@users.noreply.github.com>
2022-10-06 13:10:32 +0000

    Thu, Oct 6, 2022, 1:10 PM +00:00

--- week4/recover/recover.c
+++ week4/recover/recover.c
@@ -10,7 +10,7 @@ int main(int argc, char *argv[])
     }
 
     const int BLOCK_SIZE = 512;
-    FILE *file = fopen(argv[1], "r");
+    FILE *raw_file = fopen(argv[1], "r");
     while (fread(buffer, 1, BLOCK_SIZE, raw_file) == BLOCK_SIZE)
     {
 
