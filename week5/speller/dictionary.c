b90da6c14d284a4b63885bdc65deaeaec0287d30
heddxh <63106571+heddxh@users.noreply.github.com>
2022-12-07 11:39:58 +0000

    Wed, Dec 7, 2022, 11:39 AM +00:00

--- week5/speller/dictionary.c
+++ week5/speller/dictionary.c
@@ -31,7 +31,7 @@ bool check(const char *word)
 unsigned int hash(const char *word)
 {
     // TODO: Improve this hash function
-    for
+    for(int i = 0;i)
     return toupper(word[0]) - 'A';
 }
 
