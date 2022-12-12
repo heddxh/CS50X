975b6e7c79fabaaae85a6d3f4f84066a02f830bc
heddxh <63106571+heddxh@users.noreply.github.com>
2022-12-12 01:47:21 +0000

    Mon, Dec 12, 2022, 1:47 AM +00:00

--- week5/speller/dictionary.c
+++ week5/speller/dictionary.c
@@ -83,10 +83,8 @@ bool load(const char *dictionary)
     }
     else
     {
-        hash(dict)
-        return true;
+        while ()
     }
-
 }
 
 // Returns number of words in dictionary if loaded, else 0 if not yet loaded
