2526ae8a85a36d8f619ad7c39b34a9ef970313b4
heddxh <63106571+heddxh@users.noreply.github.com>
2022-12-25 11:35:37 +0000

    Sun, Dec 25, 2022, 11:35 AM +00:00

--- week5/test.c
+++ week5/test.c
@@ -2,9 +2,10 @@
 
 int main(void)
 {
+    char buffer[3];
     FILE *file = fopen("hi.txt", "r");
     while (fscanf(file, "%s", buffer) != EOF)
     {
-        
+        printf("%s\n", buffer);
     }
 }
