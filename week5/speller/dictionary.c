7ff220f60aa8a7007e689873a4abe1dbeb0840b5
heddxh <63106571+heddxh@users.noreply.github.com>
2022-12-27 08:11:00 +0000

    Tue, Dec 27, 2022, 8:11 AM +00:00

--- week5/speller/dictionary.c
+++ week5/speller/dictionary.c
@@ -52,7 +52,7 @@ bool check(const char *word)
 // Hashes word to a number
 unsigned int hash(const char *word)
 {
-    return (int) toupper(word[0]) - (int) "A";
+    return toupper(word[0]) - "A";
 }
 
 /*
