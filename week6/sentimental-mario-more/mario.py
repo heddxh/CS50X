882ac455ba8f7b92e259fe23c86c06aac1c40919
heddxh <63106571+heddxh@users.noreply.github.com>
2023-01-08 06:01:01 +0000

    Sun, Jan 8, 2023, 6:01 AM +00:00

--- week6/sentimental-mario-more/mario.py
+++ week6/sentimental-mario-more/mario.py
@@ -1,12 +1,14 @@
 from cs50 import get_int
 
 while True:
-    n = get_int(Height: )
+    n = get_int('Height: ')
     if 0 < n < 9:
         break
     else:
         print('Please type a int between 1 and 8')
 
-for col in range(n):
-    for raw in range(n):
-        print("#", end='')
+for row in range(n):
+    print(' '*(n - row - 1), end='')
+    print('#'*(row + 1), end='')
+    print('  ', end='')
+    print('#'*(row+1))
