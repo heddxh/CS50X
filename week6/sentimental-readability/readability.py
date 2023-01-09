03117dcc691ad276d9566f3625fc9da16c6b9b47
heddxh <63106571+heddxh@users.noreply.github.com>
2023-01-09 13:20:27 +0000

    Mon, Jan 9, 2023, 1:20 PM +00:00

--- week6/sentimental-readability/readability.py
+++ week6/sentimental-readability/readability.py
@@ -1,12 +1,13 @@
 from cs50 import get_string
 
 
-def main():
-    text = get_string('Text:')
-    words = text.split()
-    count_word = len(words)
+text = get_string('Text:')
+words = text.split()
+count_word = len(words)
 
-    for i in range(count_word):
-        count_letters += len(words[i])
-
-    
+for i in range(count_word):
+    if words[i].isalpha():
+        count_letter += len(words[i])
+    elif words[i][-1] in '.?!':
+        count_letter += len(words[i]) - 1
+        count_sentence += 1
