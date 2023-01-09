77badaa53b8c4f8d0e27469643a28a17f1a5e761
heddxh <63106571+heddxh@users.noreply.github.com>
2023-01-09 08:34:06 +0000

    Mon, Jan 9, 2023, 8:34 AM +00:00

--- week6/sentimental-credit/credit.py
+++ week6/sentimental-credit/credit.py
@@ -27,7 +27,7 @@ def check_sum(card):
     for i in range(length-2, 0, -2):
         sum += int(card[i]) * 2
     print(sum)
-    for i in range(length-1, -1, -2):
+    for i in range(length-1, 0, -2):
         sum += int(card[i])
     print(sum)
     if sum % 10 == 0:
