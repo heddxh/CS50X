dda362056e815675030bb4daf14747afc565efa6
heddxh <63106571+heddxh@users.noreply.github.com>
2023-01-09 09:53:49 +0000

    Mon, Jan 9, 2023, 9:53 AM +00:00

--- week6/sentimental-credit/credit.py
+++ week6/sentimental-credit/credit.py
@@ -38,17 +38,7 @@ def check_sum(card):
             sum += temp
     print(sum)
     for i in range(length-1, 0, -2):
-        temp = int(card[i]) * 2
-        if temp > 9:
-            sum += temp % 10 + temp // 10
-        else:
-            sum += temp
-    if length % 2 == 0:
-        temp = int(card[0]) * 2
-        if temp > 9:
-            sum += temp % 10 + temp // 10
-        else:
-            sum += temp
+        sum += int(card[i])
     print(sum)
     if sum % 10 == 0:
         return True
