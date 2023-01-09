db54da09f3a25433ceca15e4d32697a0034646f1
heddxh <63106571+heddxh@users.noreply.github.com>
2023-01-09 03:26:23 +0000

    Mon, Jan 9, 2023, 3:26 AM +00:00

---
+++ week6/sentimental-credit/credit.py
@@ -0,0 +1,27 @@
+from cs50 import get_string
+
+def main():
+    len = 0
+
+def get_card():
+    while True:
+        card = get_string('Number: ')
+        if card.isdigit():
+            len = len(card)
+            return card
+
+def check_sum(card):
+    len = len(card)
+    for i in range(len-2, 0, -2):
+        sum += card[i] * 2
+    for i in range(len-1, 0, -2):
+        sum += card[i]
+    return sum
+
+def check_len(card):
+    if len == 15:
+        is_amex(card)
+    elif len == 13:
+        is_visa(card)
+    elif len == 16:
+        if is_visa
