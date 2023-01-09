87afc71aa383f07406c2ed3fd4db55039cfec9d2
heddxh <63106571+heddxh@users.noreply.github.com>
2023-01-09 13:16:28 +0000

    Mon, Jan 9, 2023, 1:16 PM +00:00

--- week6/sentimental-readability/readability.py
+++ week6/sentimental-readability/readability.py
@@ -4,7 +4,9 @@ from cs50 import get_string
 def main():
     text = get_string('Text:')
     words = text.split()
+    count_word = len(words)
+
+    for i in range(count_word):
+        count_letters += len(words[i])
+
     
-    letters =
-    words =
-    sentences =
