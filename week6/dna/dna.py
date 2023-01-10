7dcb8a65a89086286d5df7cd14d52fe25b6f6f4e
heddxh <63106571+heddxh@users.noreply.github.com>
2023-01-10 06:59:31 +0000

    Tue, Jan 10, 2023, 6:59 AM +00:00

--- week6/dna/dna.py
+++ week6/dna/dna.py
@@ -75,7 +75,8 @@ def longest_match(sequence, subsequence):
 
 def is_dict_in(d1, d2):
     '''Check if Dict d1 is in Dict d2. If so, return True'''
-    
+    if list(d1) in list(d2):
+        if 
 
 
 if __name__ == '__main__':
