881d0cb4eabedb1001cb41ba66b16f46570ab24f
Heddxh <g311571057@gmail.com>
2023-01-25 13:25:13 +0000

    Wed, Jan 25, 2023, 1:25 PM +00:00

--- week7/movies/13.sql
+++ week7/movies/13.sql
@@ -1,4 +1,4 @@
-SELECT count(distinct(name))
+SELECT DISTINCT(people.name)
   FROM stars
   JOIN people ON people.id = stars.person_id
   JOIN movies ON movies.id = stars.movie_id
@@ -7,4 +7,4 @@ SELECT count(distinct(name))
           FROM people
           JOIN stars ON stars.person_id = people.id
          WHERE people.name = "Kevin Bacon" AND birth = 1958)
-         
+   AND people.name != "Kevin Bacon";
