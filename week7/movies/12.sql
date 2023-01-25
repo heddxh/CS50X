ad3537c296fbab9aee5f0e76e763daf6cd2467dd
Heddxh <g311571057@gmail.com>
2023-01-25 12:31:17 +0000

    Wed, Jan 25, 2023, 12:31 PM +00:00

--- week7/movies/12.sql
+++ week7/movies/12.sql
@@ -5,4 +5,4 @@ SELECT title FROM people
 SELECT title FROM people
   JOIN stars ON stars.person_id = people.id
   JOIN movies ON movies.id = stars.movie_id
- WHERE name = "Helena Bonham Carter")
+ WHERE name = "Helena Bonham Carter"
