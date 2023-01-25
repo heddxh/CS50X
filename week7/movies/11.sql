895470084a395465d3bb2b68417518a5d9e8d94a
Heddxh <g311571057@gmail.com>
2023-01-25 13:18:13 +0000

    Wed, Jan 25, 2023, 1:18 PM +00:00

--- week7/movies/11.sql
+++ week7/movies/11.sql
@@ -1,1 +1,7 @@
-SELECT title FROM movies WHERE id IN (SELECT movie_id FROM stars WHERE person_id IN (SELECT id FROM people WHERE name = "Chadwick Boseman"));
+SELECT title FROM people
+  JOIN stars ON stars.people_id = people.id
+  JOIN movies ON movies.id = stars.movie_id
+  JOIN ratings ON ratings.movie_id = stars.movie_id
+ WHERE people.name = "Chadwick Boseman"
+ ORDER BY rating DESC
+ LIMIT 5;
