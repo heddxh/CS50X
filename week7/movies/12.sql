ddf4c72ff76c7905fe0aac7113ba92083e67d74d
Heddxh <g311571057@gmail.com>
2023-01-25 12:50:35 +0000

    Wed, Jan 25, 2023, 12:50 PM +00:00

--- week7/movies/12.sql
+++ week7/movies/12.sql
@@ -1,10 +1,9 @@
-SELECT title
-  FROM (
-    SELECT title FROM people AS t1
-      JOIN stars ON stars.person_id = people.id
-      JOIN movies ON movies.id = stars.movie_id
-     WHERE name = "Helena Bonham Carter"
-  )
+SELECT title FROM people
   JOIN stars ON stars.person_id = people.id
   JOIN movies ON movies.id = stars.movie_id
  WHERE name = "Johnny Depp"
+ AND title IN
+    (SELECT title FROM people
+    JOIN stars ON stars.person_id = people.id
+    JOIN movies ON movies.id = stars.movie_id
+    WHERE name = "Helena Bonham Carter")
