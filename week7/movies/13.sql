0a16600c79b0b6976f52579cbdd2d356de55bcc8
Heddxh <g311571057@gmail.com>
2023-01-25 12:59:43 +0000

    Wed, Jan 25, 2023, 12:59 PM +00:00

--- week7/movies/13.sql
+++ week7/movies/13.sql
@@ -1,1 +1,9 @@
-SELECT name FROM
+SELECT people.name
+ FROM people
+ JOIN movies 
+
+
+SELECT movie_id
+  FROM people
+  JOIN stars ON stars.person_id = people.id
+ WHERE people.name = "Kevin Bacon" AND birth = 1985
