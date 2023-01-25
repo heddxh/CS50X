d190a55a9529eb924ee533a9106f03db0085dbde
Heddxh <g311571057@gmail.com>
2023-01-25 12:52:05 +0000

    Wed, Jan 25, 2023, 12:52 PM +00:00

--- week7/movies/12.sql
+++ week7/movies/12.sql
@@ -2,7 +2,7 @@ SELECT title FROM people
   JOIN stars ON stars.person_id = people.id
   JOIN movies ON movies.id = stars.movie_id
  WHERE name = "Johnny Depp"
- AND title IN
+   AND title IN
     (SELECT title FROM people
     JOIN stars ON stars.person_id = people.id
     JOIN movies ON movies.id = stars.movie_id
