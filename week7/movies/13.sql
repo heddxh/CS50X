d555d9c4c010063a8e121e19c1de4a894d4d9363
Heddxh <g311571057@gmail.com>
2023-01-25 13:02:19 +0000

    Wed, Jan 25, 2023, 1:02 PM +00:00

--- week7/movies/13.sql
+++ week7/movies/13.sql
@@ -1,9 +1,8 @@
 SELECT people.name
- FROM people
- JOIN movies 
-
-
-SELECT movie_id
-  FROM people
-  JOIN stars ON stars.person_id = people.id
- WHERE people.name = "Kevin Bacon" AND birth = 1985
+ FROM
+ JOIN stars ON stars.movie_id
+WHERE movies.id IN
+      (SELECT movie_id
+         FROM people
+         JOIN stars ON stars.person_id = people.id
+        WHERE people.name = "Kevin Bacon" AND birth = 1985)
