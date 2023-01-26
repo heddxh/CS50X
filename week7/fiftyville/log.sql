442ccef761841b8e9d066ad7b942e2fc525bd76c
heddxh <63106571+heddxh@users.noreply.github.com>
2023-01-26 12:12:24 +0000

    Thu, Jan 26, 2023, 8:12 PM +08:00

--- week7/fiftyville/log.sql
+++ week7/fiftyville/log.sql
@@ -6,7 +6,11 @@ SELECT description FROM crime_scene_reports
  AND description like "%CS50%";
 -- Theft of the CS50 duck took place at 10:15am at the Humphrey Street bakery. Interviews were conducted today with three witnesses who were present at the time – each of their interview transcripts mentions the bakery.
 
--- 查询面包房记录
+-- 查询面包房10点到11点的记录
 SELECT * FROM bakery_security_logs
  WHERE year = 2021 AND month = 7 AND day = 28
  AND hour = 10;
+
+-- 查询三人笔录
+SELECT * FROM interviews
+ WHERE year = 2021 AND month = 7 AND day = 28;
