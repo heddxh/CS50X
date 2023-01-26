2c265d3ed94010f14cf09c56d868efa02b069a4c
heddxh <63106571+heddxh@users.noreply.github.com>
2023-01-26 12:04:30 +0000

    Thu, Jan 26, 2023, 8:04 PM +08:00

--- week7/fiftyville/log.sql
+++ week7/fiftyville/log.sql
@@ -1,4 +1,8 @@
 -- Keep a log of any SQL queries you execute as you solve the mystery.
 
+-- 找到指定的案件
 SELECT * FROM crime_scene_reports
- WHERE 
+ WHERE year = 2021 AND month = 7 AND day = 28 AND street = "Humphrey Street"
+ AND description like "%CS50%";
+
+-- 
