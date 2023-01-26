601231a0d4c59c78204f0fad9ae7cf98a7ec41a2
heddxh <63106571+heddxh@users.noreply.github.com>
2023-01-26 12:35:14 +0000

    Thu, Jan 26, 2023, 8:35 PM +08:00

--- week7/fiftyville/log.sql
+++ week7/fiftyville/log.sql
@@ -28,4 +28,8 @@ SELECT * FROM people
  WHERE license_plate IN (SELECT license_plate FROM bakery_security_logs
                           WHERE year = 2021 AND month = 7 AND day = 28 AND hour = 10
                             AND minute BETWEEN 15 AND 25)
-   AND 
+   AND id IN (SELECT person_id FROM bank_accounts
+               WHERE account_number IN(SELECT account_number FROM atm_transactions
+                                        WHERE year = 2021 AND month = 7 AND day = 28
+                                          AND atm_location = "Leggett Street"
+                                          AND transaction_type = "withdraw"));
