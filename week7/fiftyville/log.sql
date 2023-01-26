cd39f8a7d07908b459bb07e697fc23380ae50f6d
heddxh <63106571+heddxh@users.noreply.github.com>
2023-01-26 12:07:24 +0000

    Thu, Jan 26, 2023, 8:07 PM +08:00

--- week7/fiftyville/log.sql
+++ week7/fiftyville/log.sql
@@ -4,5 +4,6 @@
 SELECT * FROM crime_scene_reports
  WHERE year = 2021 AND month = 7 AND day = 28 AND street = "Humphrey Street"
  AND description like "%CS50%";
+-- Theft of the CS50 duck took place at 10:15am at the Humphrey Street bakery. Interviews were conducted today with three witnesses who were present at the time – each of their interview transcripts mentions the bakery.
 
 -- 
