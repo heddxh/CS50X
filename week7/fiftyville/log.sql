1472e22cb23d2efdf8a37c4c881dfb4c991d9b4d
heddxh <63106571+heddxh@users.noreply.github.com>
2023-01-26 12:28:46 +0000

    Thu, Jan 26, 2023, 8:28 PM +08:00

--- week7/fiftyville/log.sql
+++ week7/fiftyville/log.sql
@@ -22,3 +22,9 @@ SELECT * FROM atm_transactions
  WHERE year = 2021 AND month = 7 AND day = 28
    AND atm_location = "Leggett Street"
    AND transaction_type = "withdraw";
+
+-- 根据上述两次查询寻找共同的人
+SELECT license_plate FROM bakery_security_logs
+
+ WHERE year = 2021 AND month = 7 AND day = 28 AND hour = 10
+   AND minute BETWEEN 15 AND 25;
