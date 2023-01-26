54708025b5d5e9287abc9a2cf50c1b9ba2f364bd
heddxh <63106571+heddxh@users.noreply.github.com>
2023-01-26 12:31:13 +0000

    Thu, Jan 26, 2023, 8:31 PM +08:00

--- week7/fiftyville/log.sql
+++ week7/fiftyville/log.sql
@@ -24,7 +24,8 @@ SELECT * FROM atm_transactions
    AND transaction_type = "withdraw";
 
 -- 根据上述两次查询寻找共同的人
-SELECT license_plate FROM bakery_security_logs
-
- WHERE year = 2021 AND month = 7 AND day = 28 AND hour = 10
-   AND minute BETWEEN 15 AND 25;
+SELECT * FROM people
+ WHERE license_plate IN (SELECT license_plate FROM bakery_security_logs
+                          WHERE year = 2021 AND month = 7 AND day = 28 AND hour = 10
+                            AND minute BETWEEN 15 AND 25)
+   AND 
