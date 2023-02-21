8057bef2b2135babad25569b99668a4809659348
heddxh <63106571+heddxh@users.noreply.github.com>
2023-02-21 15:58:37 +0000

    Tue, Feb 21, 2023, 11:58 PM +08:00

--- week9/birthdays/app.py
+++ week9/birthdays/app.py
@@ -27,7 +27,9 @@ def index():
     if request.method == "POST":
 
         # TODO: Add the user's entry into the database
-        request.form
+        name = request.form.get["name"]
+        date = request.form.get["date"]
+        db.execute("INSERT)
         return redirect("/")
 
     else:
