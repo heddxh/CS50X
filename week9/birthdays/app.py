e37e80798e08295752c022a7d8cf1d75d870866a
heddxh <63106571+heddxh@users.noreply.github.com>
2023-02-22 04:04:47 +0000

    Wed, Feb 22, 2023, 12:04 PM +08:00

--- week9/birthdays/app.py
+++ week9/birthdays/app.py
@@ -29,7 +29,9 @@ def index():
         # TODO: Add the user's entry into the database
         name = request.form.get("name")
         date = request.form.get("date")
-        db.execute("INSERT INTO birthdays (?, ?)", name, date)
+        month = date[5:7]
+        day = date[8:]
+        db.execute("INSERT INTO birthdays (name, month, day) VALUES (?, ?, ?)", name, month, day)
         return redirect("/")
 
     else:
