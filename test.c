011348091db340df04a66a69940936bf27e1e24d
heddxh <63106571+heddxh@users.noreply.github.com>
2023-03-03 14:02:22 +0000

    Fri, Mar 3, 2023, 10:02 PM +08:00

--- test.c
+++ test.c
@@ -1,12 +1,20 @@
 #include <stdio.h>
 
 int is_zhishu(int number)
+// 1 表示不是质数
 {
     int half_number = number / 2;
     for(int i = 2, i < half_number, i++)
     {
         result1 = number / i;
-        result2 = number / 
-        if()
+        result2 = number / ((float)i);
+        if(result1 == result2)
+        {
+            return 1;
+        }
+        else
+        {
+            number = result1;
+        }
     }
 }
