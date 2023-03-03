551a0b0b9704d0c485daed660a06d4a7379ff4b9
heddxh <63106571+heddxh@users.noreply.github.com>
2023-03-03 13:56:08 +0000

    Fri, Mar 3, 2023, 9:56 PM +08:00

--- test.c
+++ test.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
 
-int main(int number)
+int is_(int number)
 {
     int half_number = number / 2;
     for(int i = 2, i < half_number, i++)
     {
-        number = number 
+        number_ = number / i;
+        if(number)
     }
 }
