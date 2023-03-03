c05f25accbcc58ca5d4a0f0979f94ecc528b93fd
heddxh <63106571+heddxh@users.noreply.github.com>
2023-03-03 13:57:48 +0000

    Fri, Mar 3, 2023, 9:57 PM +08:00

--- test.c
+++ test.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
 
-int is_(int number)
+int is_zhishu(int number)
 {
     int half_number = number / 2;
     for(int i = 2, i < half_number, i++)
     {
-        number_ = number / i;
-        if(number)
+        result1 = number / i;
+        result2 = number / 
+        if()
     }
 }
