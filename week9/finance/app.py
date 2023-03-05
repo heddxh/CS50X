320be8969a7003efecc1b459c43864e5867350ba
heddxh <63106571+heddxh@users.noreply.github.com>
2023-03-05 08:32:48 +0000

    Sun, Mar 5, 2023, 4:32 PM +08:00

--- week9/finance/app.py
+++ week9/finance/app.py
@@ -226,18 +226,18 @@ def register():
 
         # Ensure username is not empty
         if not request.form.get("username"):
-            return apology("must provide username", 403)
+            return apology("must provide username", 400)
 
         # Ensure username do not exist
         username = request.form.get("username")
         if db.execute("SELECT * FROM users WHERE username = ?", username):
-            return apology("username already exists", 403)
+            return apology("username already exists", 400)
 
         # Ensure password is not empty
         if not request.form.get("password"):
-            return apology("please provide password", 403)
+            return apology("please provide password", 400)
         elif not request.form.get("confirmation"):
-            return apology("please repeat your password", 403)
+            return apology("please repeat your password", 400)
 
         # Ensure confirm is the same with the password
         password = request.form.get("password")
